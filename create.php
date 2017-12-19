7d8d45affda746eac81b99d9b364a7b1d869f4f6
Emmanuel Micard <emmanuel.micard@supinternet.fr>
2017-12-19 16:58:49 +0100

    Updated create.php to create DB and copy Entities in src/

--- create.php
+++ create.php
@@ -1,7 +1,28 @@
 <?php
 
 require_once('vendor/autoload.php');
+
 use RPGManager\Utils\Parser;
+
+if (file_exists('./src/Entity')) {
+    echo "Deleting src/Entity \n";
+    $rmEntity = shell_exec('rm -rf ./src/Entity*');
+}
+echo "Copying Entities to src/Entity \n";
+$copyEntity = shell_exec('cp -R vendor\rpgmanager\rpgmanager\src\RPGManager\Entity\ src\\');
+
+$allFiles = scandir('./src/Entity');
+$files = array_diff($allFiles, array('.', '..'));
+foreach ($files as $file) {
+    $data = file_get_contents("src/Entity/" . $file);
+    $replaceStr = str_replace("RPGManager", "src", $data);
+    file_put_contents("src/Entity/" . $file, $replaceStr);
+}
+
+$createSqlDb = shell_exec('vendor\bin\doctrine orm:schema-tool:create --dump-sql');
+echo $createSqlDb;
+$createDb = shell_exec('vendor\bin\doctrine orm:schema-tool:create');
+echo "Done!";
 
 $configGame = file_get_contents('config/game.json');
 $game = json_decode($configGame, true);
