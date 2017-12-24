af7630cd2be17fc6ea63cd50e6f44c5d8f3aa226
Skipcat <cecile.huguet@supinternet.fr>
2017-12-24 18:53:25 +0100

    Pass entityManager into Game class

--- start.php
+++ start.php
@@ -1,13 +1,8 @@
 <?php
 
 require_once('vendor/autoload.php');
+require_once('./config.php');
 
 use RPGManager\Model\Game;
 
-// $configDb = file_get_contents('config/config.php');
-// $config = json_decode($configDb, true);
-
-// $configSettings = file_get_contents('config/settings.json');
-// $config = json_decode($configSettings, true);
-
-Game::startGame();
+Game::startGame($entityManager);
