eb97b030923eca46a121e2488fe2c04d645c9b18
Henri Chabannes <henri.de-chabannes@supinternet.fr>
2017-12-29 13:29:55 +0100

    Implemented OS detection.

--- create.php
+++ create.php
@@ -10,7 +10,13 @@ if (file_exists('./src/Entity')) {
     $rmEntity = shell_exec('rm -rf ./src/Entity*');
 }
 echo "Copying Entities to src/Entity \n";
-$copyEntity = shell_exec('cp -R vendor\rpgmanager\rpgmanager\src\RPGManager\Entity\ src\\');
+
+if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+    shell_exec('mkdir ./src/Entity');
+    $copyEntity = shell_exec('cp -R vendor/rpgmanager/rpgmanager/src/RPGManager/Entity/ ./src/Entity/');
+} else {
+    $copyEntity = shell_exec('cp -R vendor\rpgmanager\rpgmanager\src\RPGManager\Entity\ src\\');
+}
 
 $allFiles = scandir('./src/Entity');
 $files = array_diff($allFiles, array('.', '..'));
@@ -28,10 +34,19 @@ if ($result = $mysqli->query("SHOW TABLES")) {
     $mysqli->query('CREATE DATABASE ' . $conn['dbname']);
 }
 
-$createSqlDb = shell_exec('vendor\bin\doctrine orm:schema-tool:create --dump-sql');
-echo $createSqlDb;
-$createDb = shell_exec('vendor\bin\doctrine orm:schema-tool:create');
-echo "Done!";
+if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+    $createSqlDb = shell_exec('vendor/bin/doctrine orm:schema-tool:create --dump-sql');
+    echo $createSqlDb;
+    $createDb = shell_exec('vendor/bin/doctrine orm:schema-tool:create');
+    echo "Done!";
+} else {
+    $createSqlDb = shell_exec('vendor\bin\doctrine orm:schema-tool:create --dump-sql');
+    echo $createSqlDb;
+    $createDb = shell_exec('vendor\bin\doctrine orm:schema-tool:create');
+    echo "Done!";
+}
+
+
 
 $configGame = file_get_contents('config/burglar.json');
 $game = json_decode($configGame, true);
